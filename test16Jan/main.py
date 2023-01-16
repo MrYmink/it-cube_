addb09e4f9238492c254b1c68d4a55387ba9457b
MrYmink <122244408+MrYmink@users.noreply.github.com>
2023-01-16 10:42:33 +0800

    Add files via upload

---
+++ test16Jan/main.py
@@ -0,0 +1,21 @@
+# Задача 1
+god = int(input())
+
+if god % 100 == 0:
+    print("YES")
+else:
+    print("NO")
+
+
+
+Kraska = int(input())
+Kraska2 = int(input())
+
+if Kraska % 2 == 0:
+    if Kraska2 % 2 == 0:
+        print(Yes)
+else:
+    print(No)   
+
+
+ 
